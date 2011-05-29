9ac04bcce019d18ebdde1bf7ed07600ad27c187c
Ankur Dave <ankurdave@gmail.com>
2011-05-28 21:43:51 -0700

    Add Aquamacs config

---
+++ .aquamacs/customizations.el
@@ -0,0 +1,55 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(TeX-PDF-mode t)
+ '(ansi-color-for-comint-mode t)
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 211 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(c-basic-offset 4)
+ '(c-default-style "k&r")
+ '(cursor-type (quote (bar . 2)))
+ '(default-frame-alist (quote ((cursor-type . box) (internal-border-width . 0) (modeline . t) (fringe) (mouse-color . "black") (background-mode . light) (tool-bar-lines . 0) (menu-bar-lines . 1) (right-fringe . 8) (left-fringe . 1) (background-color . "White") (foreground-color . "Black") (font . "-apple-Menlo-medium-normal-normal-*-15-*-*-*-m-0-iso10646-1") (fontsize . 0) (font-backend ns))))
+ '(delete-selection-mode t)
+ '(dtrt-indent-mode t nil (dtrt-indent))
+ '(global-undo-tree-mode t)
+ '(ido-enable-flex-matching t)
+ '(ido-everywhere t)
+ '(ido-mode (quote both) nil (ido))
+ '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
+ '(mouse-wheel-progressive-speed nil)
+ '(mouse-wheel-scroll-amount (quote (3 ((shift) . 1) ((control)))))
+ '(mumamo-chunk-coloring 2)
+ '(ns-command-modifier (quote meta))
+ '(ns-tool-bar-display-mode (quote both) t)
+ '(ns-tool-bar-size-mode (quote regular) t)
+ '(one-buffer-one-frame-mode nil nil (aquamacs-frame-setup))
+ '(pop-up-windows nil)
+ '(require-final-newline t)
+ '(scroll-bar-mode (quote right))
+ '(scroll-conservatively 5)
+ '(show-paren-mode t)
+ '(tab-stop-list (quote (4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120)))
+ '(tab-width 4)
+ '(tool-bar-mode nil)
+ '(transient-mark-mode t)
+ '(vc-follow-symlinks t)
+ '(visual-line-mode nil t)
+ '(word-wrap t)
+ '(x-select-enable-clipboard t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "apple" :family "Menlo"))))
+ '(cursor ((t (:background "red"))))
+ '(diff-added ((t (:inherit diff-changed :foreground "#65b042"))))
+ '(diff-removed ((t (:inherit diff-changed :foreground "#cf6a4c"))))
+ '(echo-area ((((type ns)) (:family "Menlo"))))
+ '(emacs-lisp-mode-default ((t (:inherit autoface-default :height 150 :family "Menlo"))) t)
+ '(text-mode-default ((t (:inherit autoface-default :stipple nil :strike-through nil :underline nil :slant normal :weight normal :height 150 :width normal :family "Lucida Grande"))))
+ '(variable-pitch ((t (:family "Georgia")))))
