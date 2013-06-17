ec34967c1ac27e2bd09ce7eb5df775101d43e136
Ankur Dave <ankurdave@gmail.com>
2013-06-17 16:38:29 -0700

    Clean up hooks.el

--- .emacs.d/hooks.el
+++ .emacs.d/hooks.el
@@ -1,14 +1,14 @@
-;(add-hook 'LaTeX-mode-hook 'visual-line-mode)
-;(add-hook 'org-mode-hook 'visual-line-mode)
+;; Org mode
 (add-hook 'org-mode-hook 'org-indent-mode)
+
+;; LaTeX mode
 (defun save-and-compile ()
   (interactive)
   (save-buffer)
   (TeX-command "LaTeX" 'TeX-master-file -1))
+(add-hook 'LaTeX-mode-hook
+          (lambda () (local-set-key (kbd "C-x C-s") 'save-and-compile)))
 
-(add-hook 'LaTeX-mode-hook (lambda () (local-set-key (kbd "C-x C-s") 'save-and-compile)))
-
-;; Use adaptive wrap for LaTeX, to keep the indentation level of long lines
 (add-hook 'LaTeX-mode-hook 'adaptive-wrap-prefix-mode)
 
 ;; HTML mode
@@ -17,28 +17,22 @@
 
 ;; Emacs Lisp mode
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
+(add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+
+;; Scala mode
+(defun toggle-truncate-lines-on ()
+  (toggle-truncate-lines 1))
+(add-hook 'scala-mode-hook 'toggle-truncate-lines-on)
+(add-hook 'scala-mode-hook (lambda () (setq fill-column 100)))
+
+;; C-like languages
+(add-hook 'c-mode-common-hook (lambda () (subword-mode 1)))
 
 ;; Use Python mode for TARGETS files
 (setq auto-mode-alist (cons '("\\/TARGETS$" . python-mode) auto-mode-alist))
 
 ;; Use C++ mode for .h files
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-
-;; Use 80-char line wrapping
-(defun toggle-truncate-lines-on ()
-  (toggle-truncate-lines 1))
-(add-hook 'scala-mode-hook 'toggle-truncate-lines-on)
-(add-hook 'scala-mode-hook (lambda () (setq fill-column 100)))
-
-;; Jump to words within CamelCase identifiers
-(add-hook 'c-mode-common-hook (lambda () (subword-mode 1)))
-
-;; sudo+ssh for TRAMP
-(set-default 'tramp-default-proxies-alist '((".*" "\\`root\\'" "/ssh:%h:")
-                                            ("dave-server" "www-data" "/ssh:%h:")))
-
-;; Reuse the path settings of the remote account in TRAMP
-;(add-to-list 'tramp-remote-path 'tramp-own-remote-path)
 
 ;; Interpret ANSI color codes in compiles
 (require 'ansi-color)
@@ -60,11 +54,10 @@
         (trait . font-lock-type-face)
         (object . font-lock-type-face)
         (package . font-lock-preprocessor-face)))
-;; (setq ensime-sem-high-faces '())
 
 ;; Custom keys for eshell
 (defun eshell-unbind-arrow-keys ()
-   (define-key eshell-mode-map [up] 'previous-line)
+  (define-key eshell-mode-map [up] 'previous-line)
   (define-key eshell-mode-map [down] 'next-line))
 (add-hook 'eshell-mode-hook 'eshell-unbind-arrow-keys)
 
