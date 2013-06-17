5e93ae2ac555068c3b93c8ae97de98107da2e8d4
Ankur Dave <ankurdave@gmail.com>
2013-06-17 16:37:30 -0700

    Fix git behavior in eshell

--- .emacs.d/customizations.el
+++ .emacs.d/customizations.el
@@ -28,6 +28,7 @@
  '(doc-view-continuous t)
  '(eshell-buffer-shorthand nil)
  '(eshell-cmpl-cycle-completions nil)
+ '(eshell-output-filter-functions (quote (eshell-handle-ansi-color)))
  '(eshell-review-quick-commands (quote not-even-short-output))
  '(ffap-machine-p-known (quote reject))
  '(global-whitespace-mode t)
