897b95b9e3a6f3f09feac54dd6b14cd904463252
Ankur Dave <ankurdave@gmail.com>
2013-06-21 17:23:34 -0700

    Don't disable {menu,tool,scroll}-bar-mode early
    Disabling them early is a futile attempt to avoid momentary display,
    because they will always flash for a moment. Instead, disable them in
    customizations.el for simplicity.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,3 @@
-;; Turn off mouse interface early in startup to avoid momentary display
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-
 (load "~/.emacs.d/site-local.el" t)
 
 (load "~/.emacs.d/packages.el")
