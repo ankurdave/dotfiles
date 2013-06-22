03269701cc62d1e07cc335a8e0e27a80b34dab1c
Ankur Dave <ankurdave@gmail.com>
2013-06-21 17:01:49 -0700

    Set fill-column to 80
    Also includes pretty-printing of customizations.el due to Emacs
    24.3.50.1. See
    http://lists.gnu.org/archive/html/bug-gnu-emacs/2013-04/msg00287.html.

--- .emacs.d/customizations.el
+++ .emacs.d/customizations.el
@@ -6,9 +6,16 @@
  '(TeX-PDF-mode t)
  '(TeX-save-query nil)
  '(TeX-view-program-list (quote (("open" "open -a Skim %o"))))
- '(TeX-view-program-selection (quote (((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi") (output-pdf "open") (output-html "xdg-open"))))
+ '(TeX-view-program-selection
+   (quote
+    (((output-dvi style-pstricks)
+      "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "open")
+     (output-html "xdg-open"))))
  '(ansi-color-for-comint-mode t)
- '(ansi-color-names-vector ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(aquamacs-additional-fontsets nil t)
  '(aquamacs-customization-version-id 211 t)
  '(aquamacs-tool-bar-user-customization nil t)
@@ -19,9 +26,11 @@
  '(column-number-mode t)
  '(cs164-basic-offset 2)
  '(cs164-indent-automatically t)
- '(cursor-type (quote (bar . 2)) t)
+ '(cursor-type (quote (bar . 2)))
  '(custom-enabled-themes nil)
- '(custom-safe-themes (quote ("6615e5aefae7d222a0c252c81aac52c4efb2218d35dfbb93c023c4b94d3fa0db" "c793221e18b9e52ffa82cf09ee6398322293e486a80ac7ea6458e4f0da04ac0c" "54d1bcf3fcf758af4812f98eb53b5d767f897442753e1aa468cfeb221f8734f9" "36afe64261e1de73fcfadedf154e4bc2c9ec1969bde0c21798d31366897bc4d2" "69349beba557a65bb06f89b28b8fd2890c742f07" "1440d751f5ef51f9245f8910113daee99848e2c0" "485737acc3bedc0318a567f1c0f5e7ed2dfde3fb" default)))
+ '(custom-safe-themes
+   (quote
+    ("6615e5aefae7d222a0c252c81aac52c4efb2218d35dfbb93c023c4b94d3fa0db" "c793221e18b9e52ffa82cf09ee6398322293e486a80ac7ea6458e4f0da04ac0c" "54d1bcf3fcf758af4812f98eb53b5d767f897442753e1aa468cfeb221f8734f9" "36afe64261e1de73fcfadedf154e4bc2c9ec1969bde0c21798d31366897bc4d2" "69349beba557a65bb06f89b28b8fd2890c742f07" "1440d751f5ef51f9245f8910113daee99848e2c0" "485737acc3bedc0318a567f1c0f5e7ed2dfde3fb" default)))
  '(debug-on-error nil)
  '(default-input-method "TeX")
  '(delete-by-moving-to-trash t)
@@ -32,6 +41,7 @@
  '(eshell-cmpl-cycle-completions nil)
  '(eshell-review-quick-commands (quote not-even-short-output))
  '(ffap-machine-p-known (quote reject))
+ '(fill-column 80)
  '(global-auto-revert-mode t)
  '(global-auto-revert-non-file-buffers t)
  '(global-whitespace-mode t)
@@ -58,7 +68,10 @@
  '(org-startup-folded nil)
  '(org-startup-truncated nil)
  '(osx-key-mode nil)
- '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("melpa" . "http://melpa.milkbox.net/packages/"))))
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa" . "http://melpa.milkbox.net/packages/"))))
  '(pop-up-windows t)
  '(py-indent-offset 2)
  '(python-indent-offset 2)
@@ -66,11 +79,15 @@
  '(require-final-newline t)
  '(savehist-mode t)
  '(scroll-bar-mode nil)
- '(semantic-default-submodes (quote (global-semantic-highlight-func-mode global-semantic-decoration-mode global-semantic-stickyfunc-mode global-semantic-idle-completions-mode global-semantic-idle-scheduler-mode global-semanticdb-minor-mode global-semantic-mru-bookmark-mode)))
+ '(semantic-default-submodes
+   (quote
+    (global-semantic-highlight-func-mode global-semantic-decoration-mode global-semantic-stickyfunc-mode global-semantic-idle-completions-mode global-semantic-idle-scheduler-mode global-semanticdb-minor-mode global-semantic-mru-bookmark-mode)))
  '(shift-select-mode nil)
  '(show-paren-delay 0.0)
  '(show-paren-mode t)
- '(tab-stop-list (quote (4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120)))
+ '(tab-stop-list
+   (quote
+    (4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120)))
  '(tab-width 4)
  '(tool-bar-mode nil)
  '(tramp-default-method "ssh")
