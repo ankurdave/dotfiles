7b8fb67bc99fe6910e81c88c08bc7fdc0f2135eb
Ankur Dave <ankurdave@gmail.com>
2016-03-30 17:56:57 -0700

    macrop -> fboundp for compatibility

--- .emacs.d/compat.el
+++ .emacs.d/compat.el
@@ -1,4 +1,4 @@
-(when (not (macrop 'with-eval-after-load))
+(when (not (fboundp 'with-eval-after-load))
   (defmacro with-eval-after-load (file &rest body)
     "Execute BODY after FILE is loaded.
 FILE is normally a feature name, but it can also be a file name,
