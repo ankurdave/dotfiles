67095a418e0e32394ac54d8bc09b7b1506ba02ae
Ankur Dave <ankurdave@gmail.com>
2016-10-12 17:36:18 -0700

    Fix fresh install

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;(package-initialize)
+
 (load "~/.emacs.d/site-local.el" t)
 (load "~/.emacs.d/packages.el")
 (load "~/.emacs.d/misc.el")
