fdee6e197e9ea75a8d7ff75f280442ff8c6ad38d
Ankur Dave <ankurdave@gmail.com>
2022-07-09 20:04:05 -0400

    Avoid re-encoding using pyav

--- bin/detect-birds-live.py
+++ bin/detect-birds-live.py
@@ -17,7 +17,7 @@
 # macOS:
 # $ brew install ffmpeg
 
-# $ pip3 install opencv-python torch torchvision yolov5
+# $ pip3 install opencv-python torch torchvision yolov5 onnx onnxruntime ffmpeg-python av
 
 import cv2
 import torch
@@ -27,6 +27,11 @@ import numpy as np
 import random
 import subprocess
 import datetime
+import av
+import ffmpeg
+import queue
+import threading
+import signal
 
 # device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # print(f'Using device {device}')
@@ -37,21 +42,20 @@ import datetime
 # pip3 install -r requirements.txt coremltools onnx onnx-simplifier onnxruntime openvino-dev tensorflow-cpu
 # python3 export.py --weights yolov5s.pt --include onnx
 model = yolov5.load('repos/yolov5/yolov5n.onnx')
-model.conf = 0.6
+# model = yolov5.load('/Users/ankur.dave/exp12/weights/best.pt')
+model.conf = 0.5
 # model.to(device)
 
 target_classes = ['bird', 'cat']
 
-def score_frame(frame):
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+def score_frame(frame_rgb):
     results = model(frame_rgb)
     # results.print()
     labels, cord = results.xyxyn[0][:, -1].numpy(), results.xyxyn[0][:, :-1].numpy()
 
     has_bird = False
-    confidence = 0.0
     n = len(labels)
-    x_shape, y_shape = frame.shape[1], frame.shape[0]
+    x_shape, y_shape = frame_rgb.shape[1], frame_rgb.shape[0]
     for i in range(n):
         row = cord[i]
         score = row[4]
@@ -59,78 +63,105 @@ def score_frame(frame):
         y1 = int(row[1]*y_shape)
         x2 = int(row[2]*x_shape)
         y2 = int(row[3]*y_shape)
-        bgr = (0, 255, 0) # color of the box
         classes = model.names # Get the name of label index
-        label_font = cv2.FONT_HERSHEY_SIMPLEX #Font for the label.
-        # print(classes[int(labels[i])] + ' ' + f'{score:.2f}')
-        cv2.rectangle(frame, \
-                      (x1, y1), (x2, y2), \
-                       bgr, 2) #Plot the boxes
-        cv2.putText(frame,\
-                    classes[int(labels[i])] + ' ' + f'{score:.2f}', \
-                    (x1, y1), \
-                    label_font, 0.9, bgr, 2) #Put a label over box.
         has_bird = has_bird or classes[int(labels[i])] in target_classes
-        if classes[int(labels[i])] in target_classes:
-            confidence = max(confidence, score)
 
-    return frame, has_bird, confidence
+    return has_bird
+
+in_filename = "foo"
+
+width = 2304
+height = 1296
+
+container = av.open(in_filename)
+in_stream = container.streams.video[0]
 
 now = datetime.datetime.now().strftime("%Y-%m-%dT%H%M%S")
 output_file = f"live-{now}.mp4"
+output = av.open(output_file, "w")
+out_stream = output.add_stream(template=in_stream)
+print(f"Writing to {output}")
 
-mjpeg = "http://192.168.1.143:8080/video"
-jpeg = "http://192.168.1.143:8080/shot.jpg"
-use_mjpeg = True
+target_ts_delta_per_frame = int(1 / (in_stream.time_base * in_stream.guessed_rate))
+print(f"Stream time_base={in_stream.time_base}, guessed_rate={in_stream.guessed_rate}, target_delta = {target_ts_delta_per_frame}")
 
-frame_idx = 0
-last_seen_frame_idx = -10000
+def stop_threads():
+    detect_queue.put(None)
 
-if use_mjpeg:
-    stream = cv2.VideoCapture(mjpeg)
-    if not stream.isOpened():
-        print(f"Could not open {f}")
-        quit()
-    width = int(stream.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(stream.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    out = cv2.VideoWriter(output_file, cv2.VideoWriter_fourcc(*"mp4v"), 20, \
-                          (width, height))
-    print(f"Writing to {output_file}, {width} x {height}")
+default_sigint_handler = signal.getsignal(signal.SIGINT)
+def sigint_handler(sig, frame):
+    stop_threads()
+    default_sigint_handler()
+signal.signal(signal.SIGINT, sigint_handler)
 
-while True:
-    frame_idx += 1
+max_queue_size = 50
+detect_queue = queue.Queue(maxsize=max_queue_size)
+def detector():
+    chunk = []
+    # Can have 3 values: [None, True, False].
+    # - None: need to continue detecting on subsequent frames.
+    # - True: already detected in a past frame.
+    # - False: decided to skip detection for all remaining frames in this chunk.
+    should_write_chunk = None
+    last_written_frame_dts = 0
+    last_written_frame_pts = 0
+    while True:
+        packet = detect_queue.get()
+        if packet is None: return
+        qsize = detect_queue.qsize()
+        if packet.is_keyframe:
+            # Flush the previous chunk to the output if necessary.
+            if should_write_chunk:
+                for packet2 in chunk:
+                    packet2.stream = out_stream
+                    last_written_frame_dts += target_ts_delta_per_frame
+                    last_written_frame_pts += target_ts_delta_per_frame
+                    packet2.dts = last_written_frame_dts
+                    packet2.pts = last_written_frame_pts
+                    output.mux(packet2)
 
-    start_time = time.time()
-    if not use_mjpeg:
-        stream = cv2.VideoCapture(jpeg)
-        if not stream.isOpened():
-            print(f"Could not open {f}")
-            time.sleep(5)
+            # Start a new chunk.
+            chunk = []
+            should_write_chunk = None
+
+        chunk.append(packet)
+
+        if should_write_chunk:
+            print(f"packet pts={packet.pts}: skipping detection, bird already detected. qsize={qsize}")
             continue
-    ret, frame = stream.read()
-    if not ret:
-        print(f"Could not read frame from {f}")
-        continue
-    fetch_s = time.time() - start_time
 
-    start_time = time.time()
-    # To reduce resource usage, check for bird only every 30 frames.
-    run_inference = frame_idx % 30 == 0
-    if run_inference:
-        _, has_bird, _ = score_frame(frame)
-        if has_bird:
-            last_seen_frame_idx = frame_idx
-    infer_s = time.time() - start_time
+        if (qsize > random.randint(0, max_queue_size - 1) and not packet.is_keyframe) or should_write_chunk is False:
+            print(f"packet pts={packet.pts}: skipping detection, need to catch up. qsize={qsize}")
+            should_write_chunk = False
+            continue
 
-    # After seeing a bird, record the next 60 frames.
-    start_time = time.time()
-    if frame_idx - last_seen_frame_idx < 60:
-        out.write(frame)
-    write_s = time.time() - start_time
+        start_time = time.time()
+        packet_has_bird = False
+        frames = packet.decode()
+        if len(frames) == 0: continue
+        for frame in frames:
+            frame_arr = frame.to_ndarray(format='rgb24')
+            assert frame_arr.shape == (height, width, 3), frame_arr.shape
+            has_bird = score_frame(frame_arr)
+            if has_bird:
+                packet_has_bird = True
+                should_write_chunk = True
+                break
+        infer_s = time.time() - start_time
+        print(f"packet pts={packet.pts}: has_bird={'Y' if packet_has_bird else 'n'}, infer_s={infer_s:.2f}. qsize={qsize}")
 
-    print(f"frame={frame_idx:08d}: has_bird={'?' if not run_inference else ('Y' if has_bird else 'n')}, fetch_s={fetch_s:.2f}, infer_s={infer_s:.2f}, write_s={write_s:.2f}")
+detect_thread = threading.Thread(target=detector, name='detect')
+detect_thread.start()
 
-    if not use_mjpeg:
-        # When no bird, throttle framerate to reduce resource usage.
-        if frame_idx - last_seen_frame_idx >= 120:
-            time.sleep(2)
+try:
+    for packet in container.demux(in_stream):
+        # We need to skip the "flushing" packets that `demux` generates.
+        if packet.dts is None:
+            continue
+        try:
+            detect_queue.put(packet, block=False)
+        except queue.Full:
+            print(f"Dropping packet {packet}, detect queue is full")
+finally:
+    stop_threads()
+    detect_thread.join()
