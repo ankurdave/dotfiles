cb1341621536e168341b7c802b6b4f7f143a8569
Ankur Dave <ankurdave@gmail.com>
2023-10-20 15:54:20 -0400

    Don't sort and format entries

--- .emacs.d/packages.el
+++ .emacs.d/packages.el
@@ -67,11 +67,7 @@
   :quelpa (beancount
            :fetcher github
            :repo "beancount/beancount-mode")
-  :mode ("\\.beancount\\'" . beancount-mode)
-  :config
-  (defun beancount-sort-and-format-before-save ()
-    (add-hook 'before-save-hook #'sort-and-format-beancount-entries nil t))
-  (add-hook 'beancount-mode-hook #'beancount-sort-and-format-before-save))
+  :mode ("\\.beancount\\'" . beancount-mode))
 
 (use-package c++-mode
   :ensure cc-mode
